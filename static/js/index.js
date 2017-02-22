e865961e122200c584aa727b34ab092aff22760a
bili <panning20@gmail.com>
2017-02-22 15:32:58 +0800

    add mousemove interaction

--- static/js/index.js
+++ static/js/index.js
@@ -1,35 +1,46 @@
-var mosaic = new Vue({
-    el: '.app',
-    data: {
-        cvs: null,
-        ctx: null,
-        size: 30
-    },
-    computed: {
-    },
-    mounted: function() {
-        this.cvs = document.getElementById('m');
-        this.ctx = this.cvs.getContext('2d');
-        this.cvs.width = parseInt(this.cvs.offsetWidth);
-        this.cvs.height = parseInt(this.cvs.offsetHeight);
-        this.r = this.cvs.width / this.size;
-        var colors = chroma.scale(['#00ff9d', '#b2ff00', '#333']).colors(1000);
-        var x = y = 0;
-        for (var i = 0, l = this.size; i < l; i++) {
-            for (var j = 0, l = this.size; j < l; j++) {
-                drawBlock.bind(this)(colors[_.rand(0, colors.length-1)], i, j);
-            }
-        }
-    }
+//var colors = chroma.scale(['#98FB98', '#32CD32']).colors(1000);
+VanillaTilt.init(document.querySelector('.layout-center'), {
+    max: 25,
+    speed: 1500
 });
-function drawBlock(c, x, y) {
-    var ctx = this.ctx;
+var cvs1 = document.getElementById('m1');
+var ctx1 = cvs1.getContext('2d');
+
+//var cvs2 = document.getElementById('m2');
+//var ctx2 = cvs2.getContext('2d');
+
+cvs1.width = parseInt(cvs1.offsetWidth);
+cvs1.height = parseInt(cvs1.offsetHeight);
+
+//cvs2.width = parseInt(cvs2.offsetWidth);
+//cvs2.height = parseInt(cvs2.offsetHeight);
+
+var size1 = 30;
+//var size2 =  cvs2.width / cvs1.width * size1;
+
+var r = cvs1.width / size1;
+//var delay = 10;
+
+var colors = chroma.scale(['#ffffff', '#eee']).colors(1000);
+
+for (var i = 0, l = size1; i < l; i++) {
+    for (var j = 0, l = size1; j < l; j++) {
+        drawBlock(colors[_.rand(0, colors.length-1)], i, j, ctx1, r);
+    }
+}
+//colors = chroma.scale(['#feffef', '#fdffc1', '#32CD32']).colors(1000);
+//for (var i = 0, l = size2; i < l; i++) {
+    //for (var j = 0, l = size2; j < l; j++) {
+        //drawBlock(colors[_.rand(0, colors.length-1)], i, j, ctx2, r);
+    //}
+//}
+function drawBlock(c, x, y, ctx, r) {
     ctx.save();
     ctx.fillStyle = c;
-    ctx.fillRect(x*this.r, y*this.r, this.r, this.r);
+    ctx.fillRect(x*r, y*r, r, r);
     ctx.restore();
 }
-function clearCanvas() {
-    this.cvs.width = parseInt(this.cvs.offsetWidth);
-    this.cvs.height = parseInt(this.cvs.offsetHeight);
+function clearCanvas(cvs) {
+    cvs.width = parseInt(cvs.offsetWidth);
+    cvs.height = parseInt(cvs.offsetHeight);
 }
